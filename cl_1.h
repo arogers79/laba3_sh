d79f4c7197eccf4cdc90e122ffab1074ed90c0d2
arogers79 <56653763+arogers79@users.noreply.github.com>
2020-02-18 19:01:54 +0300

    Create cl_1.h

---
+++ cl_1.h
@@ -0,0 +1,11 @@
+#ifndef CL_1_H
+#define CL_1_H
+#include "cl_base.h"
+
+class cl_1 :public cl_base {
+public:
+	static void signal_1(string&) {};
+	static void signal_2(string&) {};
+};
+
+#endif // CL_1_H
