c2fcc8448929dd586630f5f9ca1fe0bb1e381212
arogers79 <56653763+arogers79@users.noreply.github.com>
2020-02-18 19:02:24 +0300

    Create cl_2.cpp

---
+++ cl_2.cpp
@@ -0,0 +1,10 @@
+#include "iostream"
+#include "cl_2.h"
+
+void cl_2::hendler_1(string& s_text) {
+	cout << "Object name: " << get_object_name() << " Text: " << s_text;
+}
+
+void cl_2::hendler_2(string& s_text) {
+	cout << "Object name: " << get_object_name() << " Text: " << s_text;
+}
