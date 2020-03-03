211347dfe0760d9e6a1451f0e4b7eec905708464
arogers79 <56653763+arogers79@users.noreply.github.com>
2020-03-03 09:48:48 +0300

    Update cl_base.h

--- cl_base.h
+++ cl_base.h
@@ -21,6 +21,7 @@ public:
 	void emit_signal(TYPE_SIGNAL p_signal, string& s_command);
 	void set_object_name(string s_object_name);
 	string get_object_name();
+				//Иерархия объектов 				
 	void set_parent(cl_base* p_parent);
 	void add_child(cl_base* p_child);
 	void delete_child(string s_object_name);
@@ -29,8 +30,10 @@ public:
 	cl_base* get_object(string);
 	cl_base* take_child(string s_object_name);
 	cl_base* get_child(string s_object_name);
+				//Состояние объекта
 	void set_state(int i_state);
 	int get_state();
+				// сервис
 	void show_object_state();
 	void show_tree(cl_base*, int);
 
@@ -42,12 +45,12 @@ private:
 	multimap<TYPE_SIGNAL, o_sh*> connects;
 	multimap<TYPE_SIGNAL, o_sh*>::iterator it_connects;
 
-	vector<cl_base*> children;
+	vector<cl_base*> children; // ссылки на подчиненных объектов
 	vector<cl_base*>::iterator it_child;
 
-	string object_name;
-	cl_base* p_parent;
-	int i_state;
+	string object_name; // наименование объекта
+	cl_base* p_parent; // ссылка на головной объект
+	int i_state; // состояние объекта
 	void show_state_next(cl_base* ob_parent);
 };
 
